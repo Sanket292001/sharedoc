7e3613c9b293dd390dc1a717d8a4029ecff65ee1
MayurSMahajan <mayur23smahajan@gmail.com>
2022-11-03 16:07:11 +0530

    separate bottom nav as component

--- src/screens/DocsScreen.jsx
+++ src/screens/DocsScreen.jsx
@@ -1,15 +1,9 @@
 import * as React from "react";
 import AppBar from "../components/AppBar/AppBar";
 import MyDocTile from "../components/DocTile/MyDocTile";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import QrCodeIcon from "@mui/icons-material/QrCode";
-import HomeIcon from "@mui/icons-material/Home";
-import CropFreeIcon from "@mui/icons-material/CropFree";
-import Paper from "@mui/material/Paper";
-
 import "./DocsScreen.css";
 import MainButton from "../components/Buttons/MainButton";
+import BottomNavBar from "../components/BottomNav/BottomNavigation";
 
 const appBarHeading = "My Documents";
 
@@ -18,7 +12,6 @@ const editDoc = () => {
 };
 
 function DocsScreen() {
-  const [value, setValue] = React.useState(0);
 
   return (
     <div className="docs-screen-body">
@@ -33,37 +26,7 @@ function DocsScreen() {
         <MyDocTile docName="Dinner Card" editDocument={editDoc} />
       </div>
       <MainButton title="Upload A New Doc"/>
-      <Paper
-        sx={{ position: "fixed", bottom: 0, left: 0, right: 0, backgroundColor:"#333", opacity:.9125 }}
-        elevation={3}
-      >
-        <BottomNavigation
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-            //In the future based on the value of this newValue we will navigate to the next page.
-            console.log(newValue);
-          }}
-        >
-          <BottomNavigationAction
-            sx={{ color: "black" }}
-            selected
-            label="Home"
-            icon={<HomeIcon />}
-          />
-          <BottomNavigationAction
-            sx={{ color: "black" }}
-            label="Show QR"
-            icon={<QrCodeIcon />}
-          />
-          <BottomNavigationAction
-            sx={{ color: "black" }}
-            label="Scan QR"
-            icon={<CropFreeIcon />}
-          />
-        </BottomNavigation>
-        
-      </Paper>
+      <BottomNavBar/>
     </div>
   );
 }
